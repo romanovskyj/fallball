7a8333dc1ff3a7ebf061ebc7d8ce155954ab9e97
eromanovskyj <romedik@gmail.com>
2016-03-21 18:40:33 +0300

    reseller delete functionality is implemented

--- filesharing/fs/filesharing/views.py
+++ filesharing/fs/filesharing/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from filesharing.serializers import ResellerSerializer, CompanySerializer
 from filesharing.models import Reseller, Company
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from filesharing.forms import ResellerChangeForm
+from .auxiliary import marked_elements
 
 class ResellerViewSet(viewsets.ModelViewSet):
     queryset = Reseller.objects.all()
@@ -13,7 +14,19 @@ class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
 
 def resellers(request):
+
     resellers = Reseller.objects.all()
+
+    if request.method == 'POST':
+        collected_form = ResellerChangeForm(data=request.POST,resellers=resellers)
+
+        if collected_form.is_valid():
+            form_data = collected_form.cleaned_data
+            chosen_reseller_ids = marked_elements(form_data)
+
+            for r_id in chosen_reseller_ids:
+                reseller = get_object_or_404(Reseller,pk=r_id)
+                reseller.delte()
 
     return render(request, 'ui/resellers.html', {'resellers': resellers})
 
