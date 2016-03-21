255f08592f08d8c116266874089771fdd748d14b
eromanovskyj <romedik@gmail.com>
2016-03-21 18:42:03 +0300

    reseller delete functionality is implemented

---
+++ filesharing/fs/filesharing/forms.py
@@ -0,0 +1,10 @@
+__author__ = 'eromanovskyj'
+
+from django import forms
+
+class ResellerChangeForm(forms.Form):
+
+    def __init__(self, resellers, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for reseller in resellers:
+            self.fields['mark_%d' % reseller.id] = forms.BooleanField(required=False)
