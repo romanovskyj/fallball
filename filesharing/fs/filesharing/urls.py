9a281aefa6942d6ee7ff317007cdc244264ccbb1
eromanovskyj <romedik@gmail.com>
2016-03-22 11:53:53 +0300

    fast commit

--- filesharing/fs/filesharing/urls.py
+++ filesharing/fs/filesharing/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^resellers/$', views.resellers, name='resellers_list'),
     url(r'^resellers/new/', views.resellerCreate, name='resellers_new'),
     url(r'^resellers/(?P<reseller_id>[0-9]+)/$', views.reseller, name='reseller'),
-    url(r'^resellers/(?P<reseller_id>[0-9]+)/delete$', views.deleteReseller),
-    url(r'^resellers/(?P<reseller_id>[0-9]+)/new$', views.companyCreate),
+    url(r'^resellers/(?P<reseller_id>[0-9]+)/delete/$', views.deleteReseller),
+    url(r'^resellers/(?P<reseller_id>[0-9]+)/new/$', views.companyCreate),
     url(r'^resellers/(?P<reseller_id>[0-9]+)/(?P<company_id>[0-9]+)/deletey$', views.deleteCompany),
 ]
