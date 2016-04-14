30ee5b2f79add0309975b52dcf9680e72f9b5920
eromanovskyj <romedik@gmail.com>
2016-04-14 15:34:05 +0600

    fixed naming issues, implemented get_usage method

--- fallball/fb/serializers.py
+++ fallball/fb/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 from .models import Client, Reseller, User
 
 class ResellerSerializer(serializers.HyperlinkedModelSerializer):
+    usage = serializers.SerializerMethodField('get_usage')
+
     class Meta:
         model = Reseller
-        fields = ('name', 'usage', 'limit', 'access_token')
+        fields = ('instance_id', 'usage', 'limit', 'token')
+
+    def get_usage(self, obj):
+        return obj.get_usage()
