b480a1801b7c8343280be99df099bacbd7dd3c94
eromanovskyj <romedik@gmail.com>
2016-06-24 11:05:50 +0300

    fixed issue with fallball package reload. applied flake8

--- fallball/fallball/runtests.py
+++ fallball/fallball/runtests.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
-from imp import reload
 
 import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-import fallball
-
-
-# As parent directory has also 'fallball' name it needs to specify the package explicitly:
-if __name__ is not '__main__':
-    current_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.insert(0, os.path.join(current_path, 'fallball'))
-    reload(fallball)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'fallball.settings'
 django.setup()
